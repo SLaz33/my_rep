4f65f4c0414230be5db0c86f3a0b26d4907d2485
SLaz33 <723444@gmail.com>
2022-02-09 22:34:14 +0300

    test commit

---
+++ QAA_SK/2.2.6.py
@@ -0,0 +1,27 @@
+import math
+from selenium import webdriver
+import time
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+try:
+    link = "https://suninjuly.github.io/execute_script.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+    x_element = browser.find_element_by_id("input_value")
+    x = int(x_element.text)
+    y = calc(x)
+    browser.execute_script("window.scrollBy(0, 100);")
+    input1 = browser.find_element_by_id("answer")
+    input1.send_keys(y)
+    option1 = browser.find_element_by_id("robotCheckbox")
+    option1.click()
+    option2 = browser.find_element_by_id("robotsRule")
+    option2.click()
+    button = browser.find_element_by_class_name("btn")
+    button.click()
+
+finally:
+  time.sleep(10)
+  browser.quit()
