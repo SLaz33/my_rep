dba36c0f827b9dab77683c0cf5d92013978d0e43
SLaz33 <723444@gmail.com>
2022-02-09 22:58:48 +0300

    Это коммит для задания 3.1.11

---
+++ QAA_SK/2.1.7.py
@@ -0,0 +1,27 @@
+import math
+from selenium import webdriver
+import time
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+try:
+    link = "https://suninjuly.github.io/get_attribute.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+    x_element = browser.find_element_by_id("treasure")
+    x_num = x_element.get_attribute("valuex")
+    x = int(x_num)
+    y = calc(x)
+    input1 = browser.find_element_by_id("answer")
+    input1.send_keys(y)
+    option1 = browser.find_element_by_id("robotCheckbox")
+    option1.click()
+    option2 = browser.find_element_by_id("robotsRule")
+    option2.click()
+    button = browser.find_element_by_class_name("btn")
+    button.click()
+
+finally:
+  time.sleep(10)
+  browser.quit()
